cb2f43cc0676aa3bace7b3b610fda6cb3977a685
lda <lda220503@gmail.com>
2023-11-09 09:18:20 +0700

    first commit

---
+++ checkout.js
@@ -0,0 +1,41 @@
+const cartContainer = document.querySelector('.cart-container table')
+const orderTotal = document.querySelector('.cart-container h2')
+let items = JSON.parse(window.localStorage.getItem("cart"));
+
+var total = 0
+for(item of items){
+    const product = document.createElement("tr");
+    product.innerHTML=
+    `<td>
+        <div class="item-info">
+            <img src="`+ item.image +`">
+            <div>
+                <p class="name">`+ item.title +`</p>
+                <button type="button"><i class="fa-solid fa-trash-can"></i>Xoá</button>
+            </div>
+        </div>
+    </td>
+    <td><p>`+ item.amount +`</p></td>
+    <td class="price">`+ item.price*item.amount +`000đ</td>`
+    cartContainer.appendChild(product);
+    total += item.price*item.amount
+}
+orderTotal.outerHTML =
+    `<h2>Tổng cộng: 
+        <span class="total">`+total+`000đ</span>
+    </h2>`
+
+const orderBtn = document.querySelector('.order-form button')
+const customerName = document.querySelector('.customer-name')
+const customerPhone = document.querySelector('.customer-phone')
+const customerAddress = document.querySelector('.customer-address')
+
+orderBtn.addEventListener('click', function(){
+    if(customerName.value === '' || customerPhone.value === '' || customerAddress.value === ''){
+        alert('Vui lòng nhập đầy đủ thông tin')
+        return
+    }
+    alert('Đã đặt hàng!')
+    window.location.href = 'index.html'
+    localStorage.removeItem('cart');
+})
---
+++ login.js
@@ -0,0 +1,65 @@
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
+function resetAdminStatus(){
+  var upateAdminStat = [false, ''];
+  localStorage.setItem('adminStatus', JSON.stringify(upateAdminStat));
+}
+window.onload = resetAdminStatus();
+
+var users = window.localStorage.getItem('user') ? JSON.parse(window.localStorage.user) :[]
+
+var adminAccount = JSON.parse(localStorage.adminAcc);
+function signin(e){
+  event.preventDefault();
+
+  var email = document.querySelector('.email').value;
+  var username = document.querySelector('.username').value;
+  var password = document.querySelector('.password').value;
+
+  var user ={
+  email : email,
+  username : username,
+  password : password
+  }
+
+  users.push(user)
+  localStorage.setItem('user', JSON.stringify(users));
+
+  window.alert('Đã tạo tài khoản')
+}
+
+function login(e){
+  event.preventDefault();
+  
+  var username = document.querySelector('.log_username').value;
+  var password = document.querySelector('.log_password').value;
+  var result = document.querySelector('.result');
+  
+  for (adminAcc of adminAccount) {
+    if (username == adminAcc.username && password == adminAcc.password) {
+      var upateAdminStat = [true, adminAcc.name];
+      localStorage.setItem('adminStatus', JSON.stringify(upateAdminStat));
+      window.location.href = 'admin.html';
+      return;
+    }
+  }
+  for(savedUser of users){
+    if(username == savedUser.username && password == savedUser.password){
+        result.innerHTML = 'Đăng nhập thành công'
+        localStorage.setItem('isloggedin',username)
+        window.location.href = 'index.html'
+      }
+      else if((username != savedUser.username || password != savedUser.password)){
+        result.innerHTML = "Sai tài khoản hoặc mật khẩu !"
+      }
+  }
+}
